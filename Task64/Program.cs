441c783ffaba15956000afdef8ba172aea3294d6
DzmitryMytsko <dzmitrymytsko@gmail.com>
2023-10-20 22:50:02 +0300

    Solved tasks

---
+++ Task64/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.Write("Input number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if(N < 1)
+{
+    Console.Write($"N = {N} ->It's not natural number!");
+    return;
+}
+
+Console.Write($"N = {N} -> ");
+NaturalNumbers(N);
+
+void NaturalNumbers(int N)
+{
+
+    if (N == 1) 
+    {
+         Console.Write($"{N}");
+         return;
+    }
+    else
+    {
+        Console.Write($"{N}, ");
+        NaturalNumbers(N - 1);
+    }
+}
+
+
+
+
+
+// if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+//         else Console.Write($"{arr[i]}");
+
+
+// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Console.WriteLine("Input natural number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// NaturalNumber(number);
+
+// void NaturalNumber(int number)
+// {
+//     if (number == 0) return; // условие()
+//     NaturalNumber(number - 1);//хвостовая рекурсия
+//     Console.Write($"{number} "); // вывод метода
+// }
---
+++ Task66/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m <= 0)
+{
+    Console.Write($"Number {m} isn't natural");
+    return;
+}
+else if (n <= 0)
+{
+    Console.Write($"Number {n} isn't natural");
+    return;
+}
+
+Console.Write($"m = {m}; n = {n}. - > {RangeOfNumbers(m, n)}");
+
+int RangeOfNumbers(int m, int n)
+{
+    int sum = 0;
+
+    if (m > n)
+    {
+        sum += m + RangeOfNumbers(m - 1, n);
+    }
+    else if (m < n)
+    {
+        sum += n + RangeOfNumbers(m, n - 1);
+    }
+    else
+    {
+        return sum + m;
+    }
+    return sum;
+}
